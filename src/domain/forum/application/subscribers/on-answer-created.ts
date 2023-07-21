ac905eb79f240f4c882204a4bbc2e1f1965b1969
@rapha <rapha@MacBook-Air-de-smarters.local>
2023-07-21 13:05:16 -0300

    Enviando notificação no subscriber

--- src/domain/forum/application/subscribers/on-answer-created.ts
+++ src/domain/forum/application/subscribers/on-answer-created.ts
@@ -1,9 +1,14 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
 import { AnswerCreatedEvent } from '../../enterprise/events/answer-created-events'
+import { QuestionRepository } from '../repositories/question-repository'
+import { SendNotificationUseCase } from '@/domain/notification/application/use-cases/send-notification'
 
 export class OnAnswerCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private questionRepository: QuestionRepository,
+    private sendNotification: SendNotificationUseCase,
+  ) {
     this.setupSubscriptions()
   }
 
@@ -15,6 +20,18 @@ export class OnAnswerCreated implements EventHandler {
   }
 
   private async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {
-    console.log(answer)
+    const question = await this.questionRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (question) {
+      await this.sendNotification.execute({
+        recipientId: question.authorId.toString(),
+        title: `Nova resposta em ${question.title
+          .substring(0, 40)
+          .concat('...')}`,
+        content: answer.excerpt,
+      })
+    }
   }
 }
